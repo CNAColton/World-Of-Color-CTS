03f3883b2d71ce13dd43674140aacb51213a5257
CNAColton <CNAColton@gmail.com>
2024-09-16 17:34:50 -0400

    14

---
+++ World Of Color/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+
+    public void GameOver()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+}
