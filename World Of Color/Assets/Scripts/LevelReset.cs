e54d8cfb3f5aafe6da8f176c3cc00990cc2aa770
CNAColton <CNAColton@gmail.com>
2024-09-20 18:35:16 -0400

    done

--- World Of Color/Assets/Scripts/LevelReset.cs
+++ World Of Color/Assets/Scripts/LevelReset.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelReset : MonoBehaviour
 {
-
+    public ParticleSystem explosion;
+    
     public void GameOver()
     {
         gameObject.SetActive(false);
+        Invoke ("Reload", 2);
+        explosion.transform.position = transform.position;
+        explosion.Play();
+        SceneManager.LoadScene("Over");
+    }
+
+    void Reload()
+    {
         SceneManager.LoadScene(0);
+    }
+
+    private void Start()
+    {
+        explosion.Stop();
     }
 }
